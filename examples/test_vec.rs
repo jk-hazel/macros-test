3672ea70e6c884e54290e901b606af6c447caba8
jking999922222 <747011574@qq.com>
2024-06-11 22:08:09 +0800

    test-first

--- examples/test_vec.rs
+++ examples/test_vec.rs
@@ -4,6 +4,7 @@ use macros::test_vec;
 fn main() -> Result<()> {
     let mut v: Vec<i32> = test_vec!["1".parse()?, "2".parse()?, "3".parse()?];
     v.push(3);
+    v.push(4);
     println!("{:?}", v);
     Ok(())
 }
