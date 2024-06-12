41a5b48e281a7933629473e2712a193a2c44f0dc
jking999922222 <747011574@qq.com>
2024-06-12 22:49:54 +0800

    proc-macro-test

--- examples/test_vec.rs
+++ examples/test_vec.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use macros::test_vec;
 
 fn main() -> Result<()> {
     let mut v: Vec<i32> = test_vec!["1".parse()?, "2".parse()?, "3".parse()?];
@@ -7,4 +6,21 @@ fn main() -> Result<()> {
     v.push(4);
     println!("{:?}", v);
     Ok(())
+}
+
+// test_vec! = test_vec!{1,2,3};//Vec<i32>
+#[macro_export]
+macro_rules! test_vec {
+    () => {
+        Vec::new()
+    };
+
+    ($elem:expr; $n:expr) => {
+        std::vec::from_elem($elem, $n)
+    };
+
+    ($($x:expr),+ $(,)?) => {{
+        <[_]>::into_vec(Box::new([$($x), *]))
+    }};
+
 }
--- examples/try_test.rs
+++ examples/try_test.rs
@@ -1,10 +1,10 @@
-use std::fmt::format;
-
 use anyhow::{anyhow, Result};
 
 fn main() -> Result<()> {
-    let err = f3(f2(f1("测试")?)?);
-    print!("{:?}", err);
+    // let err = f3(f2(f1("test-err")?)?)?;
+    // println!("{}", err);
+    let err = test_try!(f3(test_try!(f2(test_try!(f1("test-err"))))));
+    println!("{}", err);
     Ok(())
 }
 
@@ -18,4 +18,15 @@ fn f2(s: impl AsRef<str>) -> Result<String> {
 
 fn f3(s: impl AsRef<str>) -> Result<String> {
     Err(anyhow!("f3:{}", s.as_ref()))
+}
+
+// ? operator how to simulate it
+#[macro_export]
+macro_rules! test_try {
+    ($expr:expr) => {
+        match $expr {
+            Ok(val) => val,
+            Err(err) => return Err(err.into()),
+        }
+    };
 }
